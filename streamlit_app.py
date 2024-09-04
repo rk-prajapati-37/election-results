b68ad4ae41501a45ed7db2afec3124d81091f79f
rk-prajapati-37 <r.k.prajapati0307@gmail.com>
2024-09-04 16:16:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
+# streamlit_app.py
 import streamlit as st
 from templates.home import home_page
 from templates.pandas_page import pandas_page
-from templates.news import news_page 
+from templates.news import news_page
 
 # Sidebar menu for navigation
 menu = st.sidebar.radio("Select a Page", ["Home", "State Results", "News"])
